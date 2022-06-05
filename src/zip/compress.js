d8210eff6cdbf5694d0072660b96df236e89a179
Piotr Setsko <lihadzey@gmail.com>
2022-06-05 15:03:31 +0300

    Implement functions in zip section

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,19 @@
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+import { createReadStream, createWriteStream } from 'fs';
+
 export const compress = async () => {
-    // Write your code here 
+  try {
+    const inputPath = './files/fileToCompress.txt';
+    const outputPath = './files/archive.gz';
+    const gzip = createGzip();
+    const input = createReadStream(inputPath);
+    const output = createWriteStream(outputPath);
+
+    await pipeline(input, gzip, output);
+  } catch (error) {
+    console.log(error);
+  }
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,19 @@
+import { createUnzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+import { createReadStream, createWriteStream } from 'fs';
+
 export const decompress = async () => {
-    // Write your code here 
+  try {
+    const inputPath = './files/archive.gz';
+    const outputPath = './files/fileToCompress.txt';
+    const unzip = createUnzip();
+    const input = createReadStream(inputPath);
+    const output = createWriteStream(outputPath);
+
+    await pipeline(input, unzip, output);
+  } catch (error) {
+    console.log(error);
+  }
 };
+
+decompress();
